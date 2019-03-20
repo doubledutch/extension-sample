d777c1054bcef1a2dc356fc901a2e7e196a0de83
Adam Liechty <adam@doubledutch.me>
2019-03-20 13:36:39 -0700

    + YellowBox.ignoreWarnings

--- mobile/index.js
+++ mobile/index.js
@@ -14,9 +14,14 @@
  * limitations under the License.
  */
 
-import { AppRegistry } from 'react-native'
+import { AppRegistry, YellowBox } from 'react-native'
 import HomeView from './src/HomeView'
 import { name as extensionName } from './extension.json'
 
+YellowBox.ignoreWarnings([
+  'Warning: Async Storage has been extracted',
+  'Warning: WebView has been extracted',
+  'Warning: View.propTypes has been deprecated',
+])
 AppRegistry.registerComponent(extensionName, () => HomeView)
 AppRegistry.registerComponent('section', () => HomeView)
